12ac0c82edf2ddd4fa048df923749a88d4b0d260
Anton Efimov <antone@oh-antone-mbp.orion.internal>
2018-04-11 12:41:09 +1200

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Bootstrap machine to prepare it for Ansible
+
+set -e
+
+# Download and install Homebrew
+
+if [ ! "$(which brew)" ]; then
+
+   echo "Install Homebrew"
+
+   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+fi
+
+# Download and install Ansible
+
+if [ ! "$(which ansible)" ]; then
+
+   echo "Install Ansible"
+
+   brew update
+
+   brew install ansible
+
+fi
+
+# ansible-playbook local.yml -K -i ./inventory/localhost.yml
