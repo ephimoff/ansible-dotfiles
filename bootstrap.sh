0eda5faabd9792f00eee34978d7e68e48dc2021a
Anton Efimov <a.ephimoff@gmail.com>
2018-05-10 11:16:44 +1200

    Reverting back to the standard log output

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,14 +19,5 @@ if [ ! "$(which ansible)" ]; then
    brew install ansible
 fi
 
-TARGET_FOLDER="$HOME/Projects"
-
-# Clone Compact logger https://github.com/octplane/ansible_stdout_compact_logger
-
-echo "Clonning Compact logger into $TARGET_FOLDER"
-cd "$TARGET_FOLDER"
-git clone https://github.com/octplane/ansible_stdout_compact_logger.git
-cd "$TARGET_FOLDER/ansible-dotfiles"
-
 
 # ansible-playbook local.yml -K -i ./inventory/localhost.yml
